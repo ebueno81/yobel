fab77511b0377c04b839530fb7d831888e54e52a
ebueno81 <69813020+ebueno81@users.noreply.github.com>
2024-11-18 16:48:26 -0500

    Update CharacterController.java

--- src/main/java/com/example/yobel/rest/CharacterController.java
+++ src/main/java/com/example/yobel/rest/CharacterController.java
@@ -3,9 +3,7 @@ package com.example.yobel.rest;
 import com.example.yobel.model.Characters;
 import com.example.yobel.service.CharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.*;
-=======
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,10 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/characters")
-<<<<<<< HEAD
 @CrossOrigin(origins = "http://localhost:4200") // Permite solicitudes desde este origen
-=======
->>>>>>> 4aba9588c989a0a543eb600a021d1e78bff68b70
 public class CharacterController {
 
     @Autowired
@@ -37,12 +32,8 @@ public class CharacterController {
     }
 
     @GetMapping("/filter")
-<<<<<<< HEAD
     public List<Characters> getCharactersByFilter(@RequestParam String name) {
         return characterService.getCharactersByFilter(name);
-=======
-    public List<Characters> getCharactersByFilter(@RequestParam String name, @RequestParam String status) {
-        return characterService.getCharactersByFilter(name, status);
->>>>>>> 4aba9588c989a0a543eb600a021d1e78bff68b70
+        
     }
 }
