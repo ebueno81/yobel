7255910e491b20d73198866c06b7694ef93d647e
ebueno81 <69813020+ebueno81@users.noreply.github.com>
2024-11-18 16:48:46 -0500

    Update CharacterController.java

--- src/main/java/com/example/yobel/rest/CharacterController.java
+++ src/main/java/com/example/yobel/rest/CharacterController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
->>>>>>> 4aba9588c989a0a543eb600a021d1e78bff68b70
 
 import java.util.List;
 
